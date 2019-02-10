53b6ed9b0f81cf9bf516a8b1ce8ea5e7881ddad2
Ihsan Batughan Yilmaz <cameloper@cameloper.com>
2019-02-11 00:07:06 +0100

    Added Vector2D class Added position attribute to the Bar class

--- src/edu/kit/informatik/Bar.java
+++ src/edu/kit/informatik/Bar.java
@@ -1,5 +1,8 @@
 package edu.kit.informatik;
 
 class Bar extends Figure {
-
+    /**
+     * On-Board Position of the Bar
+     */
+    Vector2D position;
 }
---
+++ src/edu/kit/informatik/Vector2D.java
@@ -0,0 +1,12 @@
+package edu.kit.informatik;
+
+class Vector2D {
+    /**
+     * Starting point of the vector
+     */
+    Point2D head;
+    /**
+     * Ending point of the vector
+     */
+    Point2D end;
+}
