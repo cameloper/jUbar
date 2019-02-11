db402e2bfd9bba97fb3dc9de86209b05867726f8
Ihsan Batughan Yilmaz <cameloper@cameloper.com>
2019-02-11 23:42:59 +0100

    + Added rawValue<->Command mappers

--- src/edu/kit/informatik/Command.java
+++ src/edu/kit/informatik/Command.java
@@ -37,4 +37,48 @@ enum Command {
      * Quit the program
      */
     QUIT;
+
+    /**
+     * Converts given rawValue to Command object
+     * @param rawValue String Entered input string
+     * @return Command equivalent of rawValue
+     */
+    static Command initWith(String rawValue) {
+        for (Command cmd : Command.values()) {
+            if (cmd.rawValue().equals(rawValue)) {
+                return cmd;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Gives the raw String value of this Command instance
+     * @return String rawValue of command instance
+     */
+    String rawValue() {
+        switch (this) {
+            case STATE:
+                return "state";
+            case PRINT:
+                return "print";
+            case SET_VC:
+                return "set-vc";
+            case ROLL:
+                return "roll";
+            case PLACE:
+                return "place";
+            case MOVE:
+                return "move";
+            case SHOW_RESULT:
+                return "show-result";
+            case RESET:
+                return "reset";
+            case QUIT:
+                return "quit";
+            default:
+                return null;
+        }
+    }
 }
