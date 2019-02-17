0ddd0f0a56b35fe675970358040cc870b3625a34
Ihsan Batughan Yilmaz <cameloper@cameloper.com>
2019-02-17 14:15:16 +0100

    Created the method that converts user input -> Operation object

--- src/edu/kit/informatik/Error.java
+++ src/edu/kit/informatik/Error.java
@@ -1,5 +1,12 @@
 package edu.kit.informatik;
 
 enum Error {
-    NO_INPUT
+    /**
+     * User did not enter anything
+     */
+    NO_INPUT,
+    /**
+     * User entered an invalid command
+     */
+    INVALID_COMMAND
 }
