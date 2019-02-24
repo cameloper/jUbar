77ed49f5e5d0c47346f8860cbc3f727b633401a9
Ihsan Batughan Yilmaz <cameloper@cameloper.com>
2019-02-24 14:54:11 +0100

    [B, #13]  ~ Changed isNumber method to isCoordinate

--- src/edu/kit/informatik/Operation.java
+++ src/edu/kit/informatik/Operation.java
@@ -70,7 +70,7 @@ final class Operation {
                     case SINGLE_COORDINATE:
                     case TWO_COORDINATES:
                     case OPT_NUMBER_COORDINATES:
-                        if (!Command.ParameterType.isNumber(parameter)) {
+                        if (!Command.ParameterType.isCoordinate(parameter)) {
                             return new Result<>(null, Error.INVALID_PRAMETER_FORMATTING);
                         }
                         break;
