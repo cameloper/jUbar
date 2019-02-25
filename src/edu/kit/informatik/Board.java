2b2dc8a7f2793ca42a363cb2cbc04986be8b6ab6
Ihsan Batughan Yilmaz <cameloper@cameloper.com>
2019-02-25 16:44:30 +0100

    [F, #4]  + Added new constructors and static methods that help creating a board with specific size

--- src/edu/kit/informatik/Board.java
+++ src/edu/kit/informatik/Board.java
@@ -1,12 +1,32 @@
 package edu.kit.informatik;
 
+import java.util.HashMap;
+
 class Board {
-    private Tile[][] tiles;
+    private HashMap<Point2D, Tile> tiles;
 
     /**
      * Default constructor for {@link Board}
      */
     Board() {
-        tiles = new Tile[15][10];
+        Point2D[] coordinates = Point2D.boardPoints(15, 11);
+        tiles = new HashMap<>();
+        for (Point2D point : coordinates) {
+            tiles.put(point, new Tile());
+        }
+    }
+
+    /**
+     * Gives the tile with given coordinates
+     * @param point Coordinates of the requested Tile
+     * @return Tile with given coordinates
+     */
+    public Tile getTile(Point2D point) {
+        if (point == null) {
+            return null;
+        }
+
+        return tiles.get(point);
     }
 }
+
--- src/edu/kit/informatik/Point2D.java
+++ src/edu/kit/informatik/Point2D.java
@@ -4,9 +4,54 @@ class Point2D {
     /**
      * Position on the X-Axis
      */
-    int x;
+    private int x;
     /**
      * Position on the Y-Axis
      */
-    int y;
+    private int y;
+
+    /**
+     * Default constructor for Point2D
+     * @param x Position in x-Axis
+     * @param y Position in y-Axis
+     */
+    public Point2D(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Public getter of X
+     *
+     * @return value of private variable X
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * Public getter of Y
+     *
+     * @return value of private variable Y
+     */
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * Generates an array of Point2D objects with given number of rows and columns
+     * @param width Width of the board
+     * @param height Height of the board
+     * @return One-dimensional array of Point2D objects
+     */
+    public static Point2D[] boardPoints(int width, int height) {
+        Point2D[] output = new Point2D[165];
+        for (int row = 0; row < height; row++) {
+            for (int column = 0; column < width; column++) {
+                output[row * 11 + column] = new Point2D(column, row);
+            }
+        }
+
+        return output;
+    }
 }
